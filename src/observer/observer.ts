c0b0a52b13ddf7a2a62010e250f764cdd8428beb
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-06-27 10:41:17 +0300

    Feature: observer to control every unit within game

---
+++ src/observer/observer.ts
@@ -0,0 +1,37 @@
+import { Store } from "../store";
+import { calculateCollisionDamage } from "./calculateCollisionDamage";
+import { createWarrior } from "../units";
+
+let started = true;
+
+document.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    started = !started;
+  }
+});
+
+export const observer = (
+  unit,
+  units,
+  globalCollision,
+  localCollision,
+  timestamp
+) => {
+  const state = {
+    ...Store.getInstance().getState(),
+  };
+
+  const shouldMove =
+    !globalCollision[unit.id]?.filter(
+      (u) => u.health > 0 && u.color !== unit.color
+    )?.length || globalCollision[unit.id]?.every((u) => u.color === unit.color);
+
+  if (started) {
+    if (unit.color === "gray" && shouldMove) {
+      unit.x += 1;
+    } else if (unit.color === "blue" && shouldMove) {
+      unit.x -= 1;
+  }
+
+  calculateCollisionDamage(unit, state, localCollision);
+};
